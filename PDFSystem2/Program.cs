ad2551fdecc53dd919d5ba20c6afc6a365c2cb67
bilginkilic <blgnklc@gmail.com>
2025-06-03 18:42:24 +0300

    Change project output type from Library to WinExe and add StartupObject; include Program.cs in compilation.

---
+++ PDFSystem2/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+namespace PDFSystem2
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana giriş noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainPage());
+        }
+    }
+} 
